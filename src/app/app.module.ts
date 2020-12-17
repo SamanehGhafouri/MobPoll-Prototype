689f5b6b730cb7d34799f03638a3d53edb01970f
SamanehGhafouri <samaneh2010gh@gmail.com>
2020-12-16 19:55:10 -0500

    bootstrap icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { FormComponent } from './form/form.component';
 import { FormOptionsComponent } from './form-options/form-options.component';
 import {ReactiveFormsModule} from "@angular/forms";
 import { NgbModalComponent } from './ngb-modal/ngb-modal.component';
+import { IconsComponent } from './icons/icons.component';
+import { FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
     FormOptionsComponent,
-    NgbModalComponent
+    NgbModalComponent,
+    IconsComponent
   ],
     imports: [
         BrowserModule,
         ReactiveFormsModule,
+        FontAwesomeModule,
     ],
   providers: [],
   bootstrap: [AppComponent]
